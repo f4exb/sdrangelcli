32fedbb812ad721fa42132759040a79bbba7e8cf
f4exb <f4exb06@gmail.com>
2018-07-12 15:49:48 +0200

    Created the main module info component

--- src/app/main/main.module.ts
+++ src/app/main/main.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { InfoComponent } from './info/info.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: []
+  declarations: [InfoComponent]
 })
 export class MainModule { }
