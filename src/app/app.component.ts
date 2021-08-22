2a51358c225cf02504c4a43bd9f2e2e7f53daa34
f4exb <f4exb06@gmail.com>
2021-08-23 00:27:04 +0200

    Bumped to v1.3.1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,7 +14,7 @@ import { AddDevicesetDialogComponent } from './main/add-deviceset-dialog/add-dev
 export class AppComponent {
 
   title = 'SDRangelCli';
-  version = '1.3.0';
+  version = '1.3.1';
   sdrangelURL = 'http://127.0.0.1:8091/sdrangel'; // the default URL
 
   constructor(private sdrangelUrlService: SdrangelUrlService,
