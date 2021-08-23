5dc7ac98a0f3788979121a612c700150eddb704a
f4exb <f4exb06@gmail.com>
2021-08-23 21:43:53 +0200

    Upgrade to Angular 12

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,7 +14,7 @@ import { AddDevicesetDialogComponent } from './main/add-deviceset-dialog/add-dev
 export class AppComponent {
 
   title = 'SDRangelCli';
-  version = '1.5.0';
+  version = '1.5.1';
   sdrangelURL = 'http://127.0.0.1:8091/sdrangel'; // the default URL
 
   constructor(private sdrangelUrlService: SdrangelUrlService,
